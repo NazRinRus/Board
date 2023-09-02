03b334e2f6ee1502430c3c59eeec72d91d798dff
NazRinRus <NazRinRus@gmail.com>
2023-09-02 14:03:11 +0300

    project: Board progress: at the stage of creating views

--- Board/sign/models.py
+++ Board/sign/models.py
@@ -1,19 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.db import models
+
+# Таблица для сверки пользователя с паролем
+class OneTimeCode(models.Model):
+    user = models.CharField(max_length=255)
+    code = models.CharField(max_length=10)
 
 
 class BaseRegisterForm(UserCreationForm):
     username = forms.CharField(label="Псевдоним")
-    email = forms.EmailField(label = "Email")
-    first_name = forms.CharField(label = "Имя")
-    last_name = forms.CharField(label = "Фамилия")
+    email = forms.EmailField(label="Email")
 
     class Meta:
         model = User
         fields = ("username",
-                  "first_name",
-                  "last_name",
                   "email",
                   "password1",
                   "password2", )
+
--- Board/sign/views.py
+++ Board/sign/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views.generic.edit import CreateView
 from .models import BaseRegisterForm
+from django.views import View
 
 class BaseRegisterView(CreateView):
     model = User
     form_class = BaseRegisterForm
-    success_url = '/'
+    success_url = 'sign/endsignup/'
+
+
